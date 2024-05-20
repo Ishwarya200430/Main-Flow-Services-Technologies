976e1d9116482fd0e8220e93187ea72112c18d98
KUNDURU ISHWARYA <164725009+Ishwarya200430@users.noreply.github.com>
2024-05-20 21:33:41 +0530

    Update Data science in python_task1.py

--- Data science in python_task1.py
+++ Data science in python_task1.py
@@ -14,11 +14,14 @@ my_list.remove(3)
 my_list[1]=5
 print(my_list)
 
+#output
+[1,5,4,6]
+
 
 # In[9]:
 
 
-#creating dictionary
+#creating the dictionary
 my_dict={'name':'ishu','age':'20','city':'nellore'}
 #adding element to dictionary
 my_dict['gender']='female'
@@ -28,11 +31,14 @@ del my_dict['age']
 my_dict['city']='banglore'
 print("updated dict:",my_dict)
 
+#output
+updated dict: {'name':'ishu','city':'banglore','gender':'female'}
+
 
 # In[10]:
 
 
-#cresting a set
+#creating a set
 my_set={1,2,3,4,5,6}
 #adding the element in the set
 my_set.add(6)
@@ -43,8 +49,11 @@ my_set.discard(2)
 my_set.add(20)
 print("udpated set:",my_set)
 
+#output
+updated set:{1,3,20,5,6}
 
-# In[ ]:
+
+
 
 
 
